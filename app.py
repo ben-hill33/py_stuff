6bae9a824657d1e8889fb2ee941abd1e61d9114a
BEN <benjamin.hill28@gmail.com>
2020-10-24 21:06:08 -0700

    added function demos

---
+++ app.py
@@ -0,0 +1,53 @@
+# def greet(first_name, last_name):
+#     print(f'Hi {first_name} {last_name}')
+#     print('Welcome aboard')
+
+
+# greet('Ben', 'Hill')
+
+# print(50+50)
+
+# def greet(name):
+#     print(f'Hi {name}')
+
+
+# greet('Ben')
+
+
+# def increment(number, by=1):
+#     return number + by
+
+
+# print(increment(2, 5))
+
+
+# def multiply(*numbers):
+#     total = 1
+#     for number in numbers:
+#         total *= number
+#     return total
+
+
+# print(multiply(2, 3, 4, 5))
+
+
+# creates object called DICTIONARY
+# put arguments in save_user invocation, then enter them in print
+# def save_user(**user):
+#     print(user["name"])
+
+
+# save_user(id=1, name="John", age=22)
+
+
+# def fizz_buzz(input):
+#     if (input % 3 == 0) and (input % 5 == 0):
+#         return "FizzBuzz"
+#     if input % 3 == 0:
+#         return 'Fizz'
+#     if input % 5 == 0:
+#         return 'Buzz'
+#     return input
+
+
+# print(fizz_buzz(15))
